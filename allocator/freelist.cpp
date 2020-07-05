41f7588f253d06738dc95007210ecbbab4c00712
Stiliyan Dragnev <stiliyan.dragnev@gmail.com>
2020-07-05 13:12:38 +0300

    Add the FreeList class

---
+++ allocator/freelist.cpp
@@ -0,0 +1,95 @@
+#include "freelist.hpp"
+
+#include <assert.h>
+#include <utility>
+
+
+namespace allocator
+{
+    FreeList::FreeList(FreeList&& source) :
+        FreeList{}
+    {
+        swap_contents_with(source);
+    }
+
+
+    void FreeList::swap_contents_with(FreeList& list)
+    {
+        std::swap(this->first, list.first);
+    }
+
+
+    FreeList& FreeList::operator=(FreeList&& rhs)
+    {
+        if (this != &rhs)
+        {
+            auto copy = std::move(rhs);
+            swap_contents_with(copy);
+        }
+
+        return *this;
+    }
+
+
+    void FreeList::insert(void* block)
+    {
+        previous_to(block) = value_of_pointer(nullptr);
+        next_to(block) = value_of_pointer(first);
+
+        if (!is_empty())
+        {
+            previous_to(first) = value_of_pointer(block);
+        }
+
+        first = block;
+    }
+
+
+    void FreeList::remove(void* block)
+    {
+        assert(!is_empty());
+        const auto previous = as_pointer(previous_to(block));
+
+        if (previous != nullptr)
+        {
+            next_to(previous) = next_to(block);
+        }
+        else
+        {
+            first = as_pointer(next_to(block));
+        }
+
+        const auto next = as_pointer(next_to(block));
+
+        if (next != nullptr)
+        {
+            previous_to(next) = previous_to(block);
+        }
+    }
+
+
+    void* FreeList::extract()
+    {
+        assert(!is_empty());
+        const auto result = first;
+        remove(first);
+
+        return result;
+    }
+
+
+    std::size_t FreeList::get_size() const
+    {
+        auto current = first;
+        auto size = std::size_t(0);
+
+        while (current != nullptr)
+        {
+            ++size;
+            current = as_pointer(next_to(current));
+        }
+
+        return size;
+    }
+
+}
---
+++ allocator/freelist.hpp
@@ -0,0 +1,77 @@
+#ifndef __FREE_LIST_HEADER_INCLUDED__
+#define __FREE_LIST_HEADER_INCLUDED__
+
+#include <cstdint>
+
+
+namespace allocator
+{
+    class FreeList
+    {
+        using PtrValueType = std::uintptr_t;
+
+    public:
+        FreeList() :
+            first(nullptr)
+        {
+        }
+
+        FreeList(FreeList&& source);
+        FreeList& operator=(FreeList&& rhs);
+
+        void insert(void* block);
+        void* extract();
+        void remove(void* block);
+
+        std::size_t get_size() const;
+
+        bool is_empty() const
+        {
+            return first == nullptr;
+        }
+
+    private:
+        static PtrValueType& previous_to(void* block)
+        {
+            return *as_pointer_to_ptr_values(block);
+        }
+
+        static PtrValueType* as_pointer_to_ptr_values(void* block)
+        {
+            return reinterpret_cast<PtrValueType*>(block);
+        }
+
+        static PtrValueType& next_to(void* block)
+        {
+            return *(as_pointer_to_ptr_values(block) + 1);
+        }
+
+        static PtrValueType value_of_pointer(void* p)
+        {
+            return reinterpret_cast<PtrValueType>(p);
+        }
+
+        static void* as_pointer(PtrValueType p)
+        {
+            return reinterpret_cast<void*>(p);
+        }
+
+    private:
+        void swap_contents_with(FreeList& list);
+
+    private:
+        void* first;
+    };
+
+
+    inline void insert_in(FreeList& list,
+                          void* first_block,
+                          void* second_block)
+    {
+        list.insert(first_block);
+        list.insert(second_block);
+    }
+
+}
+
+#endif // __FREE_LIST_HEADER_INCLUDED__
