57fd428bd446d0d4fc7a66be1d6df7a620cb7dca
Stiliyan Dragnev <stiliyan.dragnev@gmail.com>
2020-07-19 15:28:26 +0300

    Implement BuddyAllocator's constructor

--- allocator/buddyallocator.cpp
+++ allocator/buddyallocator.cpp
@@ -1,6 +1,12 @@
 #include "buddyallocator.hpp"
 
+#include <assert.h>
+#include <memory>
+#include <stdexcept>
 #include <utility>
+
+#include "arithmetic.hpp"
+#include "insufficientmemory.hpp"
 
 
 namespace allocator
@@ -8,6 +14,301 @@ namespace allocator
     BuddyAllocator::BuddyAllocator() :
         free_lists(nullptr)
     {
+    }
+
+
+    BuddyAllocator::BuddyAllocator(void* memory, std::size_t size)
+    {
+        verify_pointer_is_not_null(memory);
+        const auto memory_descriptor =
+            set_logical_start_size_and_levels_count(memory, size);
+        const auto free_memory_start =
+            create_data_structures(memory_descriptor);
+        initialise_data_structures(free_memory_start);
+    }
+
+
+    void BuddyAllocator::verify_pointer_is_not_null(void* memory)
+    {
+        if (memory == nullptr)
+        {
+            throw std::invalid_argument{ "Expected a pointer to memory!" };
+        }
+    }
+
+
+    BuddyAllocator::MemoryDescriptor
+    BuddyAllocator::set_logical_start_size_and_levels_count(
+        void* memory,
+        std::size_t size
+    )
+    {
+        const auto aligned_memory = first_aligned_address_within(memory, size);
+        auto actual_size = std::size_t(size - subtract(aligned_memory, memory));
+        const auto wasted_bytes_at_end = actual_size % LEAF_SIZE;
+        actual_size -= wasted_bytes_at_end;
+
+        set_size(actual_size);
+        set_start(aligned_memory, actual_size);
+        set_levels_count();
+
+        return { aligned_memory, actual_size, wasted_bytes_at_end };
+    }
+
+
+    void* BuddyAllocator::first_aligned_address_within(void* memory,
+                                                       std::size_t size)
+    {
+        const auto result = std::align(ALIGNMENT_REQUIREMENT,
+                                       LEAF_SIZE,
+                                       memory,
+                                       size);
+        if (result != nullptr)
+        {
+            return result;
+        }
+        else
+        {
+            throw InsufficientMemory{};
+        }
+    }
+
+
+    std::size_t BuddyAllocator::subtract(void* a, void* b)
+    {
+        assert(a >= b);
+
+        return value_of_pointer(a) - value_of_pointer(b);
+    }
+
+
+    void BuddyAllocator::set_size(std::size_t actual_size)
+    {
+        assert(actual_size >= LEAF_SIZE);
+        size = next_power_of_two(actual_size);
+    }
+
+
+    void BuddyAllocator::set_start(void* actual_start,
+                                   std::size_t actual_size)
+    {
+        const auto logical_memory_size = std::size_t(size - actual_size);
+        start = value_of_pointer(actual_start) - logical_memory_size;
+    }
+
+
+    void BuddyAllocator::set_levels_count()
+    {
+        levels_count = log2(size / LEAF_SIZE);
+
+        if (levels_count < MIN_LEVELS_COUNT)
+        {
+            throw InsufficientMemory{};
+        }
+    }
+
+
+    void* BuddyAllocator::create_data_structures(MemoryDescriptor memory)
+    {
+        memory = create_free_lists(memory);
+
+        return create_maps(memory);
+    }
+
+
+    BuddyAllocator::MemoryDescriptor
+    BuddyAllocator::create_free_lists(const MemoryDescriptor& memory)
+    {
+        const auto size_per_list = compute_size_per_list(memory);
+        const auto size_for_lists = levels_count * size_per_list;
+
+        if (size_for_lists < memory.size)
+        {
+            free_lists = new (memory.start) FreeList[levels_count];
+
+            return { add_to(memory.start, size_for_lists),
+                     memory.size - size_for_lists,
+                     memory.wasted_bytes_at_end };
+        }
+        else
+        {
+            throw InsufficientMemory{};
+        }
+    }
+
+
+    std::size_t BuddyAllocator::compute_size_per_list(
+        const MemoryDescriptor& memory
+    )
+    {
+        const auto size_of_list = sizeof(FreeList);
+
+        assert(size_of_list < memory.size);
+        auto first_list_end = add_to(memory.start, size_of_list);
+        auto available_space = std::size_t(memory.size - size_of_list);
+        const auto second_list = std::align(alignof(FreeList),
+                                            size_of_list,
+                                            first_list_end,
+                                            available_space);
+        assert(second_list != nullptr);
+
+        return subtract(second_list, memory.start);
+    }
+
+
+    void* BuddyAllocator::create_maps(const MemoryDescriptor& memory)
+    {
+        const auto bit_map_size = two_to_the_power_of(levels_count - 1);
+        const auto bit_map_size_in_bytes = size_in_bytes(bit_map_size);
+        const auto maps_start = determine_maps_storage(
+            2 * bit_map_size_in_bytes,
+            memory
+        );
+
+        split_map = BitMap(maps_start, bit_map_size - 1);
+        free_map = BitMap(maps_start + bit_map_size_in_bytes, bit_map_size);
+        free_map.flip(0);
+
+        return add_to(
+            memory.start,
+            maps_start == memory.start ?
+            2 * bit_map_size_in_bytes : 0
+        );
+    }
+
+
+    unsigned char* BuddyAllocator::determine_maps_storage(
+        std::size_t maps_size,
+        const MemoryDescriptor& memory
+    )
+    {
+        auto result = static_cast<void*>(nullptr);
+
+        if (maps_size <= memory.wasted_bytes_at_end)
+        {
+            result = add_to(memory.start, memory.size);
+        }
+        else if (maps_size < memory.size)
+        {
+            result = memory.start;
+        }
+        else
+        {
+            throw InsufficientMemory{};
+        }
+
+        return reinterpret_cast<unsigned char*>(result);
+    }
+
+
+    void BuddyAllocator::initialise_data_structures(
+        void* free_memory_start
+    )
+    {
+        const auto preallocated_size =
+            value_of_pointer(free_memory_start) - start;
+        const auto first_leaf = first_index_at(levels_count - 1);
+        const auto last_leaf_to_allocate =
+            first_leaf + size_in_leaves(preallocated_size) - 1;
+        preallocate_leaves_until(last_leaf_to_allocate);
+    }
+
+
+    void BuddyAllocator::preallocate_leaves_until(std::size_t leaf)
+    {
+        mark_blocks_as_allocated_until(leaf, levels_count - 1);
+
+        preallocate_leaves_parents_until(
+            parent_of(leaf),
+            leaf
+        );
+    }
+
+
+    void BuddyAllocator::mark_blocks_as_allocated_until(std::size_t index,
+                                                        std::size_t level)
+    {
+        if (is_even(to_level_index(index, level)))
+        {
+            flip_free_map_at(index);
+        }
+    }
+
+
+    void BuddyAllocator::flip_free_map_at(std::size_t index)
+    {
+        free_map.flip(free_map_index_for(index));
+    }
+
+
+    bool BuddyAllocator::free_map_at(std::size_t index) const
+    {
+        return free_map.at(free_map_index_for(index));
+    }
+
+
+    void BuddyAllocator::preallocate_leaves_parents_until(
+        std::size_t index,
+        std::size_t last_preallocated_leaf
+    )
+    {
+        auto level = int(levels_count - 2);
+        auto last_block_to_allocate = index;
+        auto last_allocated_child = last_preallocated_leaf;
+
+        while (level >= 0)
+        {
+            mark_blocks_as_split_until(last_block_to_allocate, level);
+            mark_blocks_as_allocated_until(last_block_to_allocate, level);
+            insert_free_blocks_at(level + 1,
+                                  last_allocated_child,
+                                  last_block_to_allocate);
+
+            --level;
+            last_allocated_child = last_block_to_allocate;
+            last_block_to_allocate = parent_of(last_block_to_allocate);
+        }
+    }
+
+
+    void BuddyAllocator::mark_blocks_as_split_until(std::size_t index,
+                                                    std::size_t level)
+    {
+        assert(level < levels_count - 1);
+
+        for (auto i = first_index_at(level);
+             i <= index;
+             ++i)
+        {
+            split_map.flip(i);
+        }
+    }
+
+
+    void BuddyAllocator::insert_free_blocks_at(std::size_t level,
+                                               std::size_t last_allocated_block,
+                                               std::size_t last_allocated_parent)
+    {
+        assert(level < levels_count);
+        const auto right_child = right_child_of(last_allocated_parent);
+
+        if (right_child > last_allocated_block)
+        {
+            free_lists[level].insert(
+                to_address(right_child, level)
+            );
+        }
+    }
+
+
+    void* BuddyAllocator::to_address(std::size_t index,
+                                     std::size_t level) const
+    {
+        const auto preceding_blocks_count =
+            index - first_index_at(level);
+
+        return as_pointer(start +
+                          preceding_blocks_count * size_at(level));
     }
 
 
--- allocator/buddyallocator.hpp
+++ allocator/buddyallocator.hpp
@@ -37,6 +37,23 @@ namespace allocator
         }
 
     private:
+        static void verify_pointer_is_not_null(void* memory);
+        static void* first_aligned_address_within(void* memory,
+                                                  std::size_t size);
+        static std::size_t compute_size_per_list(
+            const MemoryDescriptor& memory
+        );
+        static unsigned char* determine_maps_storage(
+            std::size_t maps_size,
+            const MemoryDescriptor& memory
+        );
+        static std::size_t subtract(void* a, void* b);
+
+        static void* add_to(void* p, std::size_t offset)
+        {
+            return reinterpret_cast<char*>(p) + offset;
+        }
+
         static PtrValueType value_of_pointer(void* p)
         {
             return reinterpret_cast<PtrValueType>(p);
@@ -47,8 +64,48 @@ namespace allocator
             return reinterpret_cast<void*>(p);
         }
 
+        static std::size_t size_in_leaves(std::size_t size_in_bytes)
+        {
+            return blocks_fitting(size_in_bytes, LEAF_SIZE);
+        }
+
+        static std::size_t free_map_index_for(std::size_t index)
+        {
+            index += index % 2;
+
+            return index / 2;
+        }
+
     private:
+        MemoryDescriptor set_logical_start_size_and_levels_count(
+            void* memory,
+            std::size_t size
+        );
+        void set_size(std::size_t actual_size);
+        void set_start(void* actual_start, std::size_t actual_size);
+        void set_levels_count();
+        void* create_data_structures(MemoryDescriptor memory);
+        MemoryDescriptor create_free_lists(const MemoryDescriptor& memory);
+        void* create_maps(const MemoryDescriptor& memory);
+        void initialise_data_structures(void* free_memory_start);
+        void preallocate_leaves_until(std::size_t leaf);
+        void mark_blocks_as_allocated_until(std::size_t index,
+                                            std::size_t level);
+        void mark_blocks_as_split_until(std::size_t index, std::size_t level);
+        void preallocate_leaves_parents_until(std::size_t index,
+                                              std::size_t last_preallocated_leaf);
+        void insert_free_blocks_at(std::size_t level,
+                                   std::size_t last_allocated_block,
+                                   std::size_t last_allocated_parent);
+        void* to_address(std::size_t index, std::size_t level) const;
+        void flip_free_map_at(std::size_t index);
+        bool free_map_at(std::size_t index) const;
         void swap_contents_with(BuddyAllocator& other);
+
+        std::size_t size_at(std::size_t level) const
+        {
+            return size / two_to_the_power_of(level);
+        }
 
         void clear()
         {
@@ -57,6 +114,7 @@ namespace allocator
 
     private:
         static const std::size_t LEAF_SIZE = 128;
+        static const std::size_t MIN_LEVELS_COUNT = 2;
         static const std::size_t ALIGNMENT_REQUIREMENT =
             alignof(std::max_align_t);
 
